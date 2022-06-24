075f6415b830afcca5567bcc4950783d5cfc6422
quek <2545199937@qq.com>
2022-06-24 22:29:28 +0800

    push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("master git3!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
